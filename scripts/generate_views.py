e406e0e3d43a1abff56b3e87c6e7dc528d9aa44d
mhubii <m.huber_1994@hotmail.de>
2023-09-21 22:32:37 +0100

    added more links

--- scripts/generate_views.py
+++ scripts/generate_views.py
@@ -12,20 +12,23 @@ meshes = [o3d.io.read_triangle_mesh(link) for link in links]
 meshes = [mesh.compute_vertex_normals() for mesh in meshes]
 
 # view mesh
-o3d.visualization.draw_geometries(meshes)
+# o3d.visualization.draw_geometries(meshes)
 
 pcd = o3d.geometry.PointCloud()
 
 for mesh in meshes:
     pcd += mesh.sample_points_poisson_disk(1000)
 
+# https://dl.acm.org/doi/abs/10.1145/1275808.1276407?casa_token=Mwn049sRLKgAAAAA:XxHEhPWb9dMoZ31Um78h1Bb1Oweg-Th3YBzQx3zeD-jG5gMJex3SNv5-wmxhywdgDP56MLKvuNQ
 # http://www.open3d.org/docs/latest/tutorial/geometry/pointcloud.html#Hidden-point-removal
 
 
 diameter = np.linalg.norm(
     np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound())
 )
-o3d.visualization.draw_geometries([pcd])
+
+# view pcd
+# o3d.visualization.draw_geometries([pcd])
 
 
 print("Define parameters used for hidden_point_removal")
