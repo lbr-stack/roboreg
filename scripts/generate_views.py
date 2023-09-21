fe5eb45e90a9f5fb3638a61c948606c535c9d99f
mhubii <m.huber_1994@hotmail.de>
2023-09-21 23:57:05 +0100

    added some comments

--- scripts/generate_views.py
+++ scripts/generate_views.py
@@ -9,7 +9,7 @@ links = [os.path.join(path, f"meshes/med7/collision/link_{i}.stl") for i in rang
 
 
 meshes = [o3d.io.read_triangle_mesh(link) for link in links]
-meshes = [mesh.compute_vertex_normals() for mesh in meshes]
+# meshes = [mesh.compute_vertex_normals() for mesh in meshes]
 
 # view mesh
 # o3d.visualization.draw_geometries(meshes)
@@ -19,9 +19,21 @@ pcd = o3d.geometry.PointCloud()
 for mesh in meshes:
     pcd += mesh.sample_points_poisson_disk(1000)
 
-# https://dl.acm.org/doi/abs/10.1145/1275808.1276407?casa_token=Mwn049sRLKgAAAAA:XxHEhPWb9dMoZ31Um78h1Bb1Oweg-Th3YBzQx3zeD-jG5gMJex3SNv5-wmxhywdgDP56MLKvuNQ
-# http://www.open3d.org/docs/latest/tutorial/geometry/pointcloud.html#Hidden-point-removal
+# talk: https://dl.acm.org/doi/abs/10.1145/1275808.1276407?casa_token=Mwn049sRLKgAAAAA:XxHEhPWb9dMoZ31Um78h1Bb1Oweg-Th3YBzQx3zeD-jG5gMJex3SNv5-wmxhywdgDP56MLKvuNQ
+# open3d: http://www.open3d.org/docs/latest/tutorial/geometry/pointcloud.html#Hidden-point-removal
+# open3d blogpost: https://towardsdatascience.com/3d-data-processing-with-open3d-c3062aadc72e
 
+# next supervisor meeting:
+#    # re-write MeshifyRobot to open3d
+#    # generate N random views, given camera position
+#    # save generated point clouds and corresponding camera positions
+#    # learn to predict camera position from point cloud using PointNet or similar
+# following:
+#    # diffusion models (spine pose estimation / robot drape generate dataset)
+#    # h est synthetic dataset (Fourier GAN)
+#    # LLMs + CLIP
+#    # RLHF
+#    # video forecasting
 
 diameter = np.linalg.norm(
     np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound())
@@ -32,7 +44,7 @@ diameter = np.linalg.norm(
 
 
 print("Define parameters used for hidden_point_removal")
-camera = [1, 0, 0]
+camera = [1, 1, 0.5]
 radius = diameter * 100
 
 print("Get all points that are visible from given view point")
