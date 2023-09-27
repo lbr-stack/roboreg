6187142bf0d34a190c6d972500cad97b027d7e1d
mhubii <m.huber_1994@hotmail.de>
2023-09-27 15:24:36 +0100

    added kbash alg

--- roboreg/instance_icp.py
+++ roboreg/instance_icp.py
@@ -3,33 +3,85 @@ from typing import List
 import torch
 
 
+def to_homogeneous(x: torch.Tensor) -> torch.Tensor:
+    """Converts a tensor of shape (..., N) to (..., N+1) by appending ones."""
+    return torch.nn.functional.pad(x, (0, 1), "constant", 1.0)
+
+
 class InstanceICP(object):
     HT: torch.Tensor
+    HT_init: torch.Tensor
 
     def __init__(self) -> None:
         HT = torch.eye(4)
+        HT_init = torch.eye(4)
 
     def __call__(
         self, targets: List[torch.Tensor], sources: List[torch.Tensor]
     ) -> torch.Tensor:
-        self._com_init(targets, sources)
+        # for each cloud compute centroid
+        target_centroids = [self._cloud_centroid(target) for target in targets]
+        source_centroids = [self._cloud_centroid(source) for source in sources]
 
-        return self.HT
+        print("Target clouds centroids:\n", target_centroids)
+        print("Source clouds centroids:\n", source_centroids)
 
-    def _com_init(
-        self, targets: List[torch.Tensor], sources: List[torch.Tensor]
-    ) -> None:
-        targets_com, sources_com = [], []
-        for target, source in zip(targets, sources):
-            targets_com.append(target.mean(dim=0))
-            sources_com.append(source.mean(dim=0))
-
-        targets_com = torch.stack(targets_com)
-        sources_com = torch.stack(sources_com)
-
-        # https://pytorch.org/docs/stable/generated/torch.linalg.lstsq.html
-        X, residuals, rank, singular_values = torch.linalg.lstsq(
-            targets_com, sources_com
+        # run Kabsh algorithm to estimate HT
+        self.HT_init = self._kabsh_algorithm(
+            torch.stack(target_centroids), torch.stack(source_centroids)
         )
 
-        print(X)
+        print("Initial HT using Kabsh algorithm:\n", self.HT_init)
+
+    # def _find_correspondences(
+    #         self, target: List[torch.Tensor], source: List[torch.Tensor]
+    # ):
+
+    def _cloud_centroid(self, cloud: torch.Tensor) -> torch.Tensor:
+        r"""Compute the centroid of a point cloud.
+
+        Args:
+            cloud: A tensor of shape (..., N, 3).
+
+        Returns:
+            The centroid of the point cloud, a tensor of shape (..., 3).
+        """
+        return torch.mean(cloud, dim=-2)
+
+    def _kabsh_algorithm(
+        self, target: torch.Tensor, source: torch.Tensor
+    ) -> torch.Tensor:
+        r"""Kabsh algorithm: https://en.wikipedia.org/wiki/Kabsch_algorithm
+
+        Args:
+            target: Target of shape (..., M, 3).
+            source: Source of shape (..., M, 3).
+        """
+        # compute centroids
+        target_centroid = self._cloud_centroid(target)
+        source_centroid = self._cloud_centroid(source)
+
+        print("Target centroid:\n", target_centroid)
+        print("Source centroid:\n", source_centroid)
+
+        # compute centered points
+        target_centered = target - target_centroid
+        source_centered = source - source_centroid
+
+        # compute covariance matrix
+        H = source_centered.transpose(-2, -1).mm(target_centered)
+
+        # compute SVD
+        U, _, V = torch.svd(H)
+
+        # compute rotation
+        R = V.mm(U.transpose(-2, -1))
+
+        # compute translation
+        t = target_centroid - R.mm(source_centroid.unsqueeze(-1)).squeeze(-1)
+
+        # compute homogeneous transformation
+        HT = torch.eye(4)
+        HT[:3, :3] = R
+        HT[:3, 3] = t
+        return HT
--- test/test_instance_icp.py
+++ test/test_instance_icp.py
@@ -5,14 +5,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import numpy as np
 import torch
+import open3d as o3d
 from common import load_data
 
 from roboreg.instance_icp import InstanceICP
 from roboreg.o3d_robot import O3DRobot
 
 
-def test_instance_icp():
-    observed_xyzs, mesh_xyzs = load_data(idcs=[0, 1], scan=False, visualize=False)
+def test_kabsh_algorithm():
+    observed_xyzs, mesh_xyzs = load_data(idcs=[0, 1, 2], scan=True, visualize=False)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -26,8 +27,103 @@ def test_instance_icp():
         )
 
     instance_icp = InstanceICP()
-    HT = instance_icp(observed_xyzs, mesh_xyzs)
+    instance_icp(observed_xyzs, mesh_xyzs)
+
+    # visualize initial homogenous transform
+    HT_init = instance_icp.HT_init
+
+    # to numpy
+    HT_init = HT_init.cpu().numpy()
+    for i in range(len(observed_xyzs)):
+        observed_xyzs[i] = observed_xyzs[i].cpu().numpy()
+        mesh_xyzs[i] = mesh_xyzs[i].cpu().numpy()
+
+    # visualize
+    observed_xyzs_pcds = [
+        o3d.geometry.PointCloud(o3d.utility.Vector3dVector(observed_xyz))
+        for observed_xyz in observed_xyzs
+    ]
+    mesh_xyzs_pcds = [
+        o3d.geometry.PointCloud(o3d.utility.Vector3dVector(mesh_xyz))
+        for mesh_xyz in mesh_xyzs
+    ]
+
+    # color
+    [
+        observed_xyzs_pcd.paint_uniform_color([1, 0.706, 0])
+        for observed_xyzs_pcd in observed_xyzs_pcds
+    ]
+    [
+        mesh_xyzs_pcd.paint_uniform_color([0, 0.651, 0.929])
+        for mesh_xyzs_pcd in mesh_xyzs_pcds
+    ]
+
+    # visualize
+    o3d.visualization.draw_geometries(observed_xyzs_pcds + mesh_xyzs_pcds)
+
+    # transform mesh
+    for i in range(len(mesh_xyzs_pcds)):
+        mesh_xyzs_pcds[i] = mesh_xyzs_pcds[i].transform(HT_init)
+
+    # visualize
+    o3d.visualization.draw_geometries(observed_xyzs_pcds + mesh_xyzs_pcds)
+
+def test_instance_icp():
+    observed_xyzs, mesh_xyzs = load_data(idcs=[0, 1, 2], scan=False, visualize=False)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # to numpy
+    for i in range(len(observed_xyzs)):
+        observed_xyzs[i] = torch.from_numpy(observed_xyzs[i]).to(
+            dtype=torch.float32, device=device
+        )
+        mesh_xyzs[i] = torch.from_numpy(mesh_xyzs[i]).to(
+            dtype=torch.float32, device=device
+        )
+
+    instance_icp = InstanceICP()
+    instance_icp(observed_xyzs, mesh_xyzs)
+
+    # visualize initial homogenous transform
+    HT_init = instance_icp.HT_init
+
+    # to numpy
+    HT_init = HT_init.cpu().numpy()
+    for i in range(len(observed_xyzs)):
+        observed_xyzs[i] = observed_xyzs[i].cpu().numpy()
+        mesh_xyzs[i] = mesh_xyzs[i].cpu().numpy()
+
+    # visualize
+    observed_xyzs_pcds = [
+        o3d.geometry.PointCloud(o3d.utility.Vector3dVector(observed_xyz))
+        for observed_xyz in observed_xyzs
+    ]
+    mesh_xyzs_pcds = [
+        o3d.geometry.PointCloud(o3d.utility.Vector3dVector(mesh_xyz))
+        for mesh_xyz in mesh_xyzs
+    ]
+
+    # color
+    [
+        observed_xyzs_pcd.paint_uniform_color([1, 0.706, 0])
+        for observed_xyzs_pcd in observed_xyzs_pcds
+    ]
+    [
+        mesh_xyzs_pcd.paint_uniform_color([0, 0.651, 0.929])
+        for mesh_xyzs_pcd in mesh_xyzs_pcds
+    ]
+
+    # visualize
+    o3d.visualization.draw_geometries(observed_xyzs_pcds + mesh_xyzs_pcds)
+
+    # transform mesh
+    for i in range(len(mesh_xyzs_pcds)):
+        mesh_xyzs_pcds[i] = mesh_xyzs_pcds[i].transform(HT_init)
+
+    # visualize
+    o3d.visualization.draw_geometries(observed_xyzs_pcds + mesh_xyzs_pcds)
 
 
 if __name__ == "__main__":
-    test_instance_icp()
+    test_kabsh_algorithm()
