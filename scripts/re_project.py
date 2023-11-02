2b8f21ef78c3e440a42cbdae74237aac3d1894de
mhubii <m.huber_1994@hotmail.de>
2023-11-02 12:40:37 +0000

    re-factored code

--- scripts/re_project.py
+++ scripts/re_project.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple
+from typing import List
 
 import cv2
 import numpy as np
@@ -14,104 +14,52 @@ from roboreg.o3d_robot import O3DRobot
 
 def render(
     robot: O3DRobot,
+    joint_state: np.ndarray,
+    intrinsic_matrix: np.ndarray,
+    extrinsic_matrix: np.ndarray,
     width: int = 640,
     height: int = 360,
-    path: str = "/home/martin/Dev/records/23_10_05_base_to_base_reg/left_low_res",
-    ht_file: str = "ht.npy",
-    joint_state_file: str = "joint_state_0.npy",
-    mask_file: str = "mask_0.png",
-    img_file: str = "img_0.png",
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    material_color: List[float] = [1.0, 1.0, 1.0, 1.0],
+    background_color: List[float] = [0.0, 0.0, 0.0, 1.0],
+) -> np.ndarray:
     # re-configure robot
-    joint_state = np.load(os.path.join(path, joint_state_file))
     robot.set_joint_positions(joint_state)
 
-    # render meshes
+    # create rendering scene
     meshes = robot.meshes
-
     render = rendering.OffscreenRenderer(width, height)
-
     mtl = o3d.visualization.rendering.MaterialRecord()
-    mtl.base_color = [1.0, 1.0, 1.0, 1.0]  # RGBA
+    mtl.base_color = material_color
     mtl.shader = "defaultUnlit"
-
-    render.scene.set_background([0.0, 0.0, 0.0, 1.0])  # RGBA
+    render.scene.set_background(background_color)
     for idx, mesh in enumerate(meshes):
         render.scene.add_geometry(f"link_{idx}", mesh, mtl)
 
-    # homogeneous -> optical -> look at
-    HT_cam_base = np.load(
-        os.path.join(path, ht_file)
-    )  # camera -> base frame (reference)
-    HT_base_cam = np.linalg.inv(HT_cam_base)  # base frame (reference / world) -> camera
-    print("HT_base_cam:\n", HT_base_cam)
-    print("quat HT_base_cam:\n", tf.quaternion_from_matrix(HT_base_cam))
-    print("trans HT_base_cam:\n", HT_base_cam[:3, 3])
-
-    # static transforms
-    HT_cam_optical = tf.quaternion_matrix([0.5, -0.5, 0.5, -0.5])  # camera -> optical
-
-    # base to optical frame
-    HT_base_optical = HT_base_cam @ HT_cam_optical  # base frame -> optical
-
-    print("HT_base_optical:\n", HT_base_optical)
-    print("quat HT_base_optical:\n", tf.quaternion_from_matrix(HT_base_optical))
-    print("trans HT_base_optical:\n", HT_base_optical[:3, 3])
-
-    # HT to look at... meshrender https://github.com/BerkeleyAutomation/meshrender
-    HT_optical_base = np.linalg.inv(HT_base_optical)
-    up = -HT_optical_base[1, :3]
-    eye = -np.linalg.inv(HT_optical_base[:3, :3]) @ HT_optical_base[:3, 3]
-
-    center = eye + HT_optical_base[2, :3]
+    # compute up eye center from extrinsic matrix
+    up = -extrinsic_matrix[1, :3]
+    eye = -np.linalg.inv(extrinsic_matrix[:3, :3]) @ extrinsic_matrix[:3, 3]
+    center = eye + extrinsic_matrix[2, :3]
 
     print("center: ", center.transpose())
     print("eye: ", eye.transpose())
     print("up: ", up.transpose())
 
-    scale_height = 640.0 / 448.0
-    scale_width = 360.0 / 256.0
-    intrinsic_matrix = np.array(
-        [
-            [184.9792022705078 * scale_height, 0.0, 222.7788848876953 * scale_height],
-            [0.0, 187.91539001464844 * scale_width, 123.90357971191406 * scale_width],
-            [0.0, 0.0, 1.0],
-        ]
-    )
-
     render.setup_camera(
         intrinsic_matrix,
-        HT_base_optical,
+        np.eye(4),  # TODO: replace setup camera function or fix it
         width,
         height,
     )
 
-    # look at
+    # render
     render.scene.camera.look_at(center, eye, up)
-
-    img_o3d = render.render_to_image()
-
-    # we can now save the rendered image right at this point
-    o3d.io.write_image("output.png", img_o3d, 9)
-
-    # plot diff
-    mask = cv2.imread(os.path.join(path, mask_file), cv2.IMREAD_GRAYSCALE)
-    mask_render = cv2.cvtColor(np.asarray(img_o3d), cv2.COLOR_RGB2GRAY)
-    mask_render = np.where(mask_render > 128, 255, 0).astype(np.uint8)
-
-    img = cv2.imread(os.path.join(path, img_file))
-
-    # pad zeros to mask
-    mask_color_render = np.stack(
-        [mask_render, np.zeros_like(mask_render), np.zeros_like(mask_render)], axis=2
-    )
-
-    overlay = cv2.addWeighted(img, 1.0, mask_color_render, 1.0, 0)
-
-    return img, mask, mask_render, overlay
+    o3d_render = render.render_to_image()
+    return np.asarray(o3d_render)
 
 
 if __name__ == "__main__":
+    input_prefix = "/home/martin/Dev/records/23_10_05_base_to_base_reg/left_low_res"
+    ht_file = "ht.npy"
     output_prefix = "/tmp/img"
 
     # load robot
@@ -123,34 +71,92 @@ if __name__ == "__main__":
 
     robot = O3DRobot(urdf)
 
+    # TODO: remove hardcoding and get proper intrinsics
+    scale_height = 640.0 / 448.0
+    scale_width = 360.0 / 256.0
+    intrinsic_matrix = np.array(
+        [
+            [184.9792022705078 * scale_height, 0.0, 222.7788848876953 * scale_height],
+            [0.0, 187.91539001464844 * scale_width, 123.90357971191406 * scale_width],
+            [0.0, 0.0, 1.0],
+        ]
+    )
+
     for i in range(8):
-        img, mask, mask_render, overlay = render(
+        ###########
+        # load data
+        ###########
+        joint_state_file = f"joint_state_{i}.npy"
+        img_file = f"img_{i}.png"
+        mask_file = f"mask_{i}.png"
+
+        extrinsic_matrix = np.load(os.path.join(input_prefix, ht_file))
+        joint_state = np.load(os.path.join(input_prefix, joint_state_file))
+        img = cv2.imread(os.path.join(input_prefix, img_file))
+        mask = cv2.imread(os.path.join(input_prefix, mask_file), cv2.IMREAD_GRAYSCALE)
+
+        ########################
+        # homogeneous -> optical
+        ########################
+        HT_cam_base = np.load(
+            os.path.join(input_prefix, ht_file)
+        )  # camera -> base frame (reference)
+        HT_base_cam = np.linalg.inv(
+            HT_cam_base
+        )  # base frame (reference / world) -> camera
+
+        # static transforms
+        HT_cam_optical = tf.quaternion_matrix(
+            [0.5, -0.5, 0.5, -0.5]
+        )  # camera -> optical
+
+        # base to optical frame
+        HT_base_optical = HT_base_cam @ HT_cam_optical  # base frame -> optical
+        HT_optical_base = np.linalg.inv(HT_base_optical)
+
+        #############
+        # render mask
+        #############
+        o3d_render = render(
             robot=robot,
+            joint_state=joint_state,
+            intrinsic_matrix=intrinsic_matrix,
+            extrinsic_matrix=HT_optical_base,
             width=640,
             height=360,
-            path="/home/martin/Dev/records/23_10_05_base_to_base_reg/left_low_res",
-            ht_file="ht.npy",
-            joint_state_file=f"joint_state_{i}.npy",
-            mask_file=f"mask_{i}.png",
-            img_file=f"img_{i}.png",
         )
 
-        diff = np.abs(mask - mask_render)
+        #################
+        # post-processing
+        #################
+        o3d_render_gray = cv2.cvtColor(o3d_render, cv2.COLOR_RGB2GRAY)
+
+        # pad zeros to mask
+        o3d_render_blue = np.stack(
+            [o3d_render_gray, np.zeros_like(o3d_render_gray), np.zeros_like(o3d_render_gray)],
+            axis=2,
+        )
+
+        overlay = cv2.addWeighted(img, 1.0, o3d_render_blue, 1.0, 0)
+
+        diff = np.abs(mask - o3d_render_gray)
 
         # resize to double size
         img = cv2.resize(img, [int(size * 4) for size in img.shape[:2][::-1]])
         mask = cv2.resize(mask, [int(size * 4) for size in mask.shape[:2][::-1]])
-        mask_render = cv2.resize(
-            mask_render, [int(size * 4) for size in mask_render.shape[:2][::-1]]
+        o3d_render_gray = cv2.resize(
+            o3d_render_gray, [int(size * 4) for size in o3d_render_gray.shape[:2][::-1]]
         )
         diff = cv2.resize(diff, [int(size * 4) for size in diff.shape[:2][::-1]])
         overlay = cv2.resize(
             overlay, [int(size * 4) for size in overlay.shape[:2][::-1]]
         )
 
+        ######
+        # save
+        ######
         cv2.imwrite(os.path.join(output_prefix, f"img_{i}.jpg"), img)
         cv2.imwrite(os.path.join(output_prefix, f"mask_{i}.jpg"), mask)
-        cv2.imwrite(os.path.join(output_prefix, f"mask_render_{i}.jpg"), mask_render)
+        cv2.imwrite(os.path.join(output_prefix, f"mask_render_{i}.jpg"), o3d_render_gray)
         cv2.imwrite(os.path.join(output_prefix, f"diff_{i}.jpg"), diff)
         cv2.imwrite(os.path.join(output_prefix, f"overlay_{i}.jpg"), overlay)
-        cv2.waitKey()
