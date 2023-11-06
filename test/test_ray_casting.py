8651bde9d15731cddc9fbb088707a1dc3b216cd3
mhubii <m.huber_1994@hotmail.de>
2023-11-06 20:58:17 +0000

    added projection

--- test/test_ray_casting.py
+++ test/test_ray_casting.py
@@ -1,9 +1,12 @@
+import cv2
 import numpy as np
 import open3d as o3d
+import torch
 import transformations as tf
 
 from roboreg.ray_cast import RayCastRobot
-from roboreg.util import generate_o3d_robot
+from roboreg.util import generate_o3d_robot, clean_xyz
+from roboreg.hydra_icp import hydra_centroid_alignment
 
 
 def test_ray_casting() -> None:
@@ -60,13 +63,9 @@ def test_ray_cast_homogeneous() -> None:
 
     cast = RayCastRobot(robot)
 
-    intrinsic_matrix = np.array(
-        [
-            [533.9981079101562, 0.0, 478.0845642089844],
-            [0.0, 533.9981079101562, 260.9956970214844],
-            [0.0, 0.0, 1.0],
-        ]
-    )
+    intrinsic_matrix = np.eye(3)
+    intrinsic_matrix[0, 0] = 640  # some values
+    intrinsic_matrix[1, 1] = 480
 
     pcd = cast.cast_ht(
         intrinsic_matrix=intrinsic_matrix,
@@ -79,6 +78,73 @@ def test_ray_cast_homogeneous() -> None:
     )
 
 
+def test_ray_cast_with_centroid_align() -> None:
+    prefix = "test/data/low_res"
+    observed_xyzs = []
+    mesh_xyzs = []
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # load robot
+    robot = generate_o3d_robot()
+
+    for idx in [0, 1, 2]:
+        # load data
+        mask = cv2.imread(f"{prefix}/mask_{idx}.png", cv2.IMREAD_GRAYSCALE)
+        observed_xyz = np.load(f"{prefix}/xyz_{idx}.npy")
+        joint_state = np.load(f"{prefix}/joint_state_{idx}.npy")
+
+        # clean cloud
+        observed_xyzs.append(
+            torch.from_numpy(clean_xyz(observed_xyz, mask)).to(device).double()
+        )
+
+        robot.set_joint_positions(joint_state)
+        pcds = robot.sample_point_clouds()
+        mesh_xyzs.append(
+            torch.from_numpy(np.concatenate([np.array(pcd.points) for pcd in pcds]))
+            .to(device)
+            .double()
+        )
+
+    HT = (
+        hydra_centroid_alignment(
+            Xs=observed_xyzs,
+            Ys=mesh_xyzs,
+        )
+        .cpu()
+        .numpy()
+    )
+
+    # to optical frame
+    HT = HT @ tf.quaternion_matrix([0.5, -0.5, 0.5, -0.5])  # to optical frame
+    HT = np.linalg.inv(HT)
+
+    # given HT, render
+    cast = RayCastRobot(robot)
+
+    width = 640
+    height = 360
+    intrinsic_matrix = np.array(
+        [
+            [263.8703308105469, 0.0, 318.25634765625],
+            [0.0, 263.8703308105469, 174.2410888671875],
+            [0.0, 0.0, 1.0],
+        ]
+    )
+
+    pcd = cast.cast_ht(
+        intrinsic_matrix=intrinsic_matrix,
+        extrinsic_matrix=HT,
+        width_px=width,
+        height_px=height,
+    )
+
+    o3d.visualization.draw_geometries(
+        [pcd.to_legacy()],
+    )
+
+
 if __name__ == "__main__":
-    test_ray_cast_homogeneous()
+    # test_ray_cast_homogeneous()
     # test_ray_casting()
+    test_ray_cast_with_centroid_align()
