2e080fd7d9ead620bf321c3723831ff7e1a8a5a3
mhubii <m.huber_1994@hotmail.de>
2023-11-07 11:12:47 +0000

    added shrink mask

--- roboreg/util.py
+++ roboreg/util.py
@@ -1,5 +1,6 @@
 import os
 
+import cv2
 import numpy as np
 import xacro
 from ament_index_python import get_package_share_directory
@@ -44,6 +45,11 @@ def extend_mask(mask: np.ndarray, kernel: np.ndarray = np.ones([10, 10])) -> np.
     extended_mask = convolve2d(mask, kernel, mode="same")
     extended_mask = np.where(extended_mask > 0.0, 255.0, 0.0).astype(np.uint8)
     return extended_mask
+
+
+def shrink_mask(mask: np.ndarray, kernel: np.ndarray = np.ones([4, 4])) -> np.ndarray:
+    shrinked_mask = cv2.erode(mask, kernel)
+    return shrinked_mask
 
 
 def generate_o3d_robot(
--- test/test_util.py
+++ test/test_util.py
@@ -1,6 +1,6 @@
 import cv2
 
-from roboreg.util import extend_mask
+from roboreg.util import extend_mask, shrink_mask
 
 
 def test_extend_mask() -> None:
@@ -11,5 +11,14 @@ def test_extend_mask() -> None:
     cv2.waitKey()
 
 
+def test_shrink_mask() -> None:
+    mask = cv2.imread("test/data/low_res/mask_0.png", cv2.IMREAD_GRAYSCALE)
+    shrinked_mask = shrink_mask(mask)
+    cv2.imshow("mask", mask)
+    cv2.imshow("shrinked_mask", shrinked_mask)
+    cv2.waitKey()
+
+
 if __name__ == "__main__":
-    test_extend_mask()
+    # test_extend_mask()
+    test_shrink_mask()
