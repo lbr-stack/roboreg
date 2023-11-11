97ae4376c0d43af6bb0f38e055e3cf60670ff7c8
mhubii <m.huber_1994@hotmail.de>
2023-11-11 23:16:27 +0000

    added optim on se(3) test

--- test/test_lie_group.py
+++ test/test_lie_group.py
@@ -1,3 +1,4 @@
+import theseus as th
 import torch
 import torchlie as lie
 
@@ -17,5 +18,56 @@ def test_lie_group():
     print(lie.SE3.exp(v))
 
 
+def test_optimizer_on_lie_group():
+    # a translation target transform
+    Th_target = lie.SE3.exp(torch.tensor([0.4, 0.5, 0.6, 0.0, 0.0, 0.0]))
+
+    # some point in R^3
+    p = torch.tensor([1.0, 0.0, 0.0]).unsqueeze(0)
+    p_prime = Th_target @ p
+
+    # setup optimization variables
+    p_var = th.Variable(p, name="p")
+    p_prime_var = th.Variable(p_prime, name="p_prime")
+    Th_vec_var = th.Vector(dof=6, name="Th_vec")
+
+    # specify cost function
+    def cost_fn(optim_vars, aux_vars):
+        Th_vec = optim_vars[0]
+        p, p_prime = aux_vars
+        Th = lie.SE3.exp(Th_vec.tensor)
+
+        p_prime_hat = Th @ p.tensor
+        err = p_prime_hat - p_prime.tensor
+        return err
+
+    # setup objective
+    objective = th.Objective()
+    cost_fn = th.AutoDiffCostFunction(
+        optim_vars=[Th_vec_var],
+        err_fn=cost_fn,
+        dim=3,
+        aux_vars=[p_var, p_prime_var],
+        cost_weight=th.ScaleCostWeight(1.0),
+        name="cost_fn",
+    )
+    objective.add(cost_fn)
+    optimizer = th.LevenbergMarquardt(objective, max_iteration=15, step_size=1.0)
+
+    layer = th.TheseusLayer(optimizer=optimizer)
+
+    with torch.no_grad():
+        output, info = layer.forward(
+            input_tensors={
+                "Th_vec": torch.zeros(6).unsqueeze(0)
+            },  # zeros as initial guess
+            optimizer_kwargs={"track_best_solution": True, "verbose": True},
+        )
+
+    print("best solution: ", info.best_solution)
+    print(output)
+
+
 if __name__ == "__main__":
-    test_lie_group()
+    # test_lie_group()
+    test_optimizer_on_lie_group()
