ecefca94daabe668c578b9f85997cad4efa082f9
mhubii <m.huber_1994@hotmail.de>
2023-11-13 15:41:38 +0000

    added a new test

---
+++ test/test_hydra_projection.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from roboreg.hydra_icp import HydraProjection
+
+
+def test_hydra_projection() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    test_hydra_projection()
