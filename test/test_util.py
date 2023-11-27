b2bc229f916bf81ea82aa427506c8e9f4c52516c
mhubii <m.huber_1994@hotmail.de>
2023-11-27 17:17:24 +0000

    updated util test

--- test/test_util.py
+++ test/test_util.py
@@ -1,10 +1,11 @@
 import cv2
 
-from roboreg.util import extend_mask, mask_boundary, shrink_mask
+from roboreg.util import extend_mask, mask_boundary, overlay_mask, shrink_mask
 
 
 def test_extend_mask() -> None:
-    mask = cv2.imread("test/data/low_res/mask_0.png", cv2.IMREAD_GRAYSCALE)
+    idx = 1
+    mask = cv2.imread(f"test/data/high_res/mask_{idx}.png", cv2.IMREAD_GRAYSCALE)
     extended_mask = extend_mask(mask)
     cv2.imshow("mask", mask)
     cv2.imshow("extended_mask", extended_mask)
@@ -12,15 +13,20 @@ def test_extend_mask() -> None:
 
 
 def test_mask_boundary() -> None:
-    mask = cv2.imread("test/data/low_res/mask_0.png", cv2.IMREAD_GRAYSCALE)
+    idx = 1
+    img = cv2.imread(f"test/data/high_res/img_{idx}.png")
+    mask = cv2.imread(f"test/data/high_res/mask_{idx}.png", cv2.IMREAD_GRAYSCALE)
     boundary_mask = mask_boundary(mask)
+    overlay = overlay_mask(img, boundary_mask, mode="b", alpha=1.0, scale=1.0)
     cv2.imshow("mask", mask)
     cv2.imshow("boundary_mask", boundary_mask)
+    cv2.imshow("overlay", overlay)
     cv2.waitKey()
 
 
 def test_shrink_mask() -> None:
-    mask = cv2.imread("test/data/low_res/mask_0.png", cv2.IMREAD_GRAYSCALE)
+    idx = 1
+    mask = cv2.imread(f"test/data/high_res/mask_{idx}.png", cv2.IMREAD_GRAYSCALE)
     shrinked_mask = shrink_mask(mask)
     cv2.imshow("mask", mask)
     cv2.imshow("shrinked_mask", shrinked_mask)
