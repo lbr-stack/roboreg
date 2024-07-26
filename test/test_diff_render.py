d7f2cd2a503c2a88e7bf6f882ef6391217aabb2c
mhubii <m.huber_1994@hotmail.de>
2024-07-27 00:33:08 +0200

    first multi render

--- test/test_diff_render.py
+++ test/test_diff_render.py
@@ -1,4 +1,5 @@
 from typing import List
+import numpy as np
 
 import torch
 import trimesh
@@ -39,6 +40,7 @@ if __name__ == "__main__":
     # Step 0: Load mesh and visualize
     mesh_0: trimesh.Geometry = trimesh.load("test/data/lbr_med7/mesh/link_0.stl")
     mesh_1: trimesh.Geometry = trimesh.load("test/data/lbr_med7/mesh/link_1.stl")
+    # meshes = [mesh_1]
     meshes = [mesh_0, mesh_1]
 
     # mesh = trimesh.load("test/data/xarm/mesh/link_base.STL")
@@ -62,44 +64,113 @@ if __name__ == "__main__":
 
     # xyz -> xyz1
     pos = torch.cat([pos, torch.ones_like(pos[:, :1])], dim=1)
-    pos = pos.unsqueeze(0)
+
+    # create ranges
+    lengths = [len(mesh.vertices) for mesh in meshes]
+    lengths = torch.tensor(lengths, device="cpu", dtype=torch.int32)
+
+    # offsets
+    offsets = torch.tensor(
+        [0] + list(np.cumsum(lengths)[:-1]), device=device, dtype=torch.int32
+    )
+
+    print("offsets")
+    print(offsets)
+    print("lenghts")
+    print(lengths)
+
+    for idx, mesh in enumerate(meshes):
+        print(f"***************************{idx}")
+        print(mesh.faces)
+
+        offset_idcs = torch.add(
+            torch.tensor(meshes[idx].faces, device=device, dtype=torch.int32),
+            offsets[idx],
+        )
+        print(offset_idcs)
 
     # FK: N_links x 4 x 4 and applied to range of vertices
     # # TODO: projections (range mode) including FK
-    pos_idx = torch.cat(
-        [torch.tensor(mesh.faces, device=device, dtype=torch.int32) for mesh in meshes],
+    pos_idx = torch.cat(  ### offset these indices by ranges
+        [
+            torch.add(
+                torch.tensor(meshes[i].faces, device=device, dtype=torch.int32),
+                offsets[i],
+            )
+            for i in range(len(meshes))
+        ],
         dim=0,
     )
 
-    # create ranges
-    ranges = []
-    previous_range = 0
-    for mesh in meshes:
-        ranges.append([previous_range, previous_range + len(mesh.faces) - 1])
-        previous_range += len(mesh.faces)
-    ranges = torch.tensor(ranges, device="cpu", dtype=torch.int32)
+    # offsets = torch.cat(
+    #     [
+    #         torch.tensor(
+    #             [offsets[i]] * len(meshes[i].faces),
+    #             device=device,
+    #             dtype=torch.int32,
+    #         )
+    #         for i in range(len(meshes))
+    #     ]
+    # )
+    print("************************+")
+    print(offsets.shape)
+
+    # replicate offsets Nx1 -> Nx3
+    offsets = torch.cat([offsets.unsqueeze(1)] * 3, dim=1)
+
+    print("offset")
+    print(pos_idx)
+
+    # pos_idx = pos_idx + offsets.unsqueeze(1)
 
     ## target resolution, target color,....
 
-    ctx = dr.RasterizeCudaContext(device=device)
-    rast, _ = dr.rasterize(ctx, pos, pos_idx, resolution=[512, 512], ranges=ranges)
-    col = torch.tensor([[1.0, 1.0, 1.0]], device=device, dtype=torch.float32).unsqueeze(
-        0
-    )
-    col_idx = torch.zeros_like(pos_idx)
-    color, _ = dr.interpolate(col, rast, col_idx)
-    color = dr.antialias(color, rast, pos, pos_idx)
-
-    # display rasterized image
+    # some dummy transform
+    import transformations
     import cv2
 
-    rast = rast.detach().cpu().numpy()
-    print(rast.shape)
-    cv2.imshow("rasterized", rast[0])
-    cv2.waitKey()
+    try:
+        cnt = 0
+        pos = pos.unsqueeze(0)
 
-    # display interpolation
-    color = color.detach().cpu().numpy()
-    print(color.shape)
-    cv2.imshow("interpolated", color[0])
-    cv2.waitKey()
+        DUMMY_HT = torch.from_numpy(
+            transformations.euler_matrix(np.pi / 2, 0.0, 0.0).astype("float32")
+        ).to(device)
+        pos = torch.matmul(pos, DUMMY_HT.t())
+        while True:
+            cnt += 1
+            DUMMY_HT = torch.from_numpy(
+                transformations.euler_matrix(0.0, 0.05, 0.0).astype("float32")
+            ).to(device)
+            pos = torch.matmul(pos, DUMMY_HT.t())
+
+            ctx = dr.RasterizeCudaContext(device=device)
+            rast, _ = dr.rasterize(
+                ctx,
+                pos,
+                pos_idx,
+                resolution=[512, 512],
+                ranges=torch.tensor(
+                    [0, 4038], device="cpu", dtype=torch.int32
+                ).unsqueeze(0),
+            )
+            col = torch.tensor(
+                [[1.0, 1.0, 1.0]], device=device, dtype=torch.float32
+            ).unsqueeze(0)
+            col_idx = torch.zeros_like(pos_idx)
+            color, _ = dr.interpolate(col, rast, col_idx)
+            color = dr.antialias(color, rast, pos, pos_idx)
+
+            # display rasterized image
+
+            # rast = rast.detach().cpu().numpy()
+            # cv2.imshow("rasterized", rast[0])
+            # cv2.waitKey()
+
+            # display interpolation
+            color = color.detach().cpu().numpy()
+            print(color.shape)
+            cv2.imshow("interpolated", color[0])
+            cv2.waitKey()
+    except KeyboardInterrupt:
+        pass
