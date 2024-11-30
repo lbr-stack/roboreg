6c837839b3b4b6a1b65caad1abf7b8f3e77bbb71
mhubii <m.huber_1994@hotmail.de>
2024-11-30 14:51:01 +0000

    re-factored particle swarm (#21)

--- roboreg/optim/particle_swarm.py
+++ roboreg/optim/particle_swarm.py
@@ -3,112 +3,161 @@ from abc import ABC, abstractmethod
 import numpy as np
 import rich
 import torch
+from rich import progress
 
 
-class ParticleSwarmOptimizer(ABC):
+class ParticleSwarm(ABC):
+    def __init__(self, particles: torch.Tensor, particle_bounds: torch.Tensor) -> None:
+        if particle_bounds.shape[-1] != 2 or particle_bounds.ndim != 2:
+            raise ValueError("Particle bounds must have shape (DoF, 2).")
+        if particle_bounds[:, 0].ge(particle_bounds[:, 1]).any():
+            raise ValueError("Lower bounds must be less than upper bounds.")
+        if particles.shape[-1] != particle_bounds.shape[0]:
+            raise ValueError(
+                "Particles and particle bounds must have the same number of DoF."
+            )
+        if particles.ndim != 2:
+            raise ValueError("Particles must be 2 dimensional.")
+
+        self._particles = particles
+        self._particle_bounds = particle_bounds
+        self._velocity = torch.zeros_like(particles)
+
+    @property
+    def particles(self) -> torch.Tensor:
+        return self._particles
+
+    @property
+    def particle_bounds(self) -> torch.Tensor:
+        return self._particle_bounds
+
+    @property
+    def velocity(self) -> torch.Tensor:
+        return self._velocity
+
+    @abstractmethod
+    def compute_velocity(self) -> None:
+        pass
+
+    def step(self) -> None:
+        self._particles += self._velocity
+        self._particles = torch.clamp(
+            self._particles,
+            min=self._particle_bounds[:, 0],
+            max=self._particle_bounds[:, 1],
+        )
+
+
+class LinearParticleSwarm(ParticleSwarm):
+    def __init__(
+        self,
+        particles: torch.Tensor,
+        particle_bounds: torch.Tensor,
+        w: float = 0.7,
+        c1: float = 1.5,
+        c2: float = 1.5,
+    ) -> None:
+        super().__init__(particles, particle_bounds)
+        self._w = w
+        self._c1 = c1
+        self._c2 = c2
+
+    def compute_velocity(
+        self,
+        best_particles: torch.Tensor,
+        best_particle: torch.Tensor,
+    ) -> None:
+        r1 = torch.rand_like(self._particles)
+        r2 = torch.rand_like(self._particles)
+        self._velocity = (
+            self._w * self._velocity
+            + self._c1 * r1 * (best_particles - self._particles)
+            + self._c2 * r2 * (best_particle - self._particles)
+        )
+
+
+class ParticleSwarmOptimizer:
     __slots__ = [
-        "_device",
-        "_random_particles",
+        "_particle_swarm",
         "_best_particles",
         "_best_particle",
         "_best_particles_fitness",
         "_best_fitness",
-        "_velocity",
     ]
 
-    def __init__(self, device: torch.device="cuda") -> None:
-        self._device = device
-        self._random_particles = self._init_random_particles()
-        if not isinstance(self._random_particles, torch.Tensor):
-            raise ValueError(
-                f"Expected _init_random_particles to return a torch.Tensor, got {type(self._random_particles)}"
-            )
-        if self._random_particles.device != self._device:
-            raise ValueError(
-                f"Expected _init_random_particles to return a tensor on device {self._device}, got {self._random_particles.device}"
-            )
+    def __init__(self, particle_swarm: ParticleSwarm) -> None:
+        self._particle_swarm = particle_swarm
         self._best_particles = None
         self._best_particle = None
         self._best_particles_fitness = None
         self._best_fitness = None
-        self._velocity = None
         self._init_state()
 
     @property
-    def device(self) -> torch.device:
-        return self._device
-    
+    def particle_swarm(self) -> ParticleSwarm:
+        return self._particle_swarm
+
     @property
     def best_particle(self) -> torch.Tensor:
         return self._best_particle
-    
+
     @property
     def best_fitness(self) -> float:
         return self._best_fitness.item()
 
-    @abstractmethod
-    def _fitness_function(self, particles: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
-
-    @abstractmethod
-    def _init_random_particles(self) -> torch.Tensor:
-        raise NotImplementedError
-
     def _init_state(self) -> None:
-        self._best_particles = self._random_particles.clone()  # B x DoF
-        self._best_particle = torch.zeros_like(self._random_particles[0])  # DoF
+        self._best_particles = self._particle_swarm.particles.clone()  # B x DoF
+        self._best_particle = torch.zeros_like(self._particle_swarm.particles[0])  # DoF
         self._best_particles_fitness = torch.full_like(
-            self._random_particles[:, 0], float("inf")
+            self._particle_swarm.particles[:, 0], float("inf")
         )  # B
         self._best_fitness = float("inf")
-        self._velocity = torch.zeros_like(self._random_particles)
 
-    @torch.no_grad()
     def __call__(
         self,
-        w: float = 0.7,
-        c1: float = 1.5,
-        c2: float = 1.5,
+        fitness_function: callable,
         max_iterations: int = 100,
         min_velocity: float = 1.0e-4,
     ) -> torch.Tensor:
         try:
-            iteration = 0
-            while iteration < max_iterations and self._velocity.mean() > min_velocity:
-                iteration += 1
+            with torch.no_grad():
+                for iteration in progress.track(
+                    range(max_iterations), description="Optimizing particle swarm..."
+                ):
+                    if (
+                        self._particle_swarm.velocity.norm() < min_velocity
+                        and iteration > 0
+                    ):
+                        rich.print(
+                            "Velocity is below threshold. Stopping optimization."
+                        )
+                        break
 
-                # compute particle veloctiy
-                r1 = torch.rand_like(self._random_particles)
-                r2 = torch.rand_like(self._random_particles)
-
-                # update particle velocity
-                self._velocity = (
-                    w * self._velocity
-                    + c1 * r1 * (self._best_particles - self._random_particles)
-                    + c2 * r2 * (self._best_particle - self._random_particles)
-                )
-
-                # update particle position
-                self._random_particles = self._random_particles + self._velocity
-
-                # evaluate fitness
-                fitnesses = self._fitness_function(self._random_particles)
-
-                # update particles best known losses and positions
-                mask = fitnesses < self._best_particles_fitness
-                self._best_particles_fitness[mask] = fitnesses[mask]
-                self._best_particles[mask] = self._random_particles[mask]
-
-                # update global best
-                best_fitness_idx = torch.argmin(self._best_particles_fitness)
-                best_fitness = self._best_particles_fitness[best_fitness_idx]
-                if best_fitness < self._best_fitness:
-                    self._best_fitness = best_fitness
-                    self._best_particle = self._best_particles[best_fitness_idx]
-                    best_loss_print = np.round(self._best_fitness.item(), 3)
-                    rich.print(
-                        f"New best loss: {best_loss_print} at iteration {iteration}."
+                    # update particle velocity
+                    self._particle_swarm.compute_velocity(
+                        self._best_particles,
+                        self._best_particle,
                     )
+                    self._particle_swarm.step()
+
+                    # evaluate fitness
+                    fitnesses = fitness_function()
+
+                    # update particles best known losses and positions
+                    mask = fitnesses < self._best_particles_fitness
+                    self._best_particles_fitness[mask] = fitnesses[mask]
+                    self._best_particles[mask] = self._particle_swarm.particles[mask]
+
+                    # update global best
+                    best_fitness_idx = torch.argmin(self._best_particles_fitness)
+                    best_fitness = self._best_particles_fitness[best_fitness_idx]
+                    if best_fitness < self._best_fitness:
+                        self._best_fitness = best_fitness
+                        self._best_particle = self._best_particles[best_fitness_idx]
+                        best_loss_print = np.round(self._best_fitness.item(), 3)
+                        rich.print(
+                            f"New best loss: {best_loss_print} at iteration {iteration}."
+                        )
         except KeyboardInterrupt:
             pass
         return self._best_particle
---
+++ test/optim/test_particle_swarm.py
@@ -0,0 +1,91 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+)
+
+import torch
+
+from roboreg.optim import LinearParticleSwarm, ParticleSwarmOptimizer
+
+
+def test_particle_swarm() -> None:
+    n_particles = 10
+    particle_dof = 7
+
+    particles = torch.rand(n_particles, particle_dof)
+    particle_bounds = torch.zeros(particle_dof, 2)
+    particle_bounds[:, 0] = -1.0
+    particle_bounds[:, 1] = 1.0
+    best_particles = torch.rand(n_particles, particle_dof)
+    best_particle = torch.rand(particle_dof)
+    swarm = LinearParticleSwarm(
+        particles=particles,
+        particle_bounds=particle_bounds,
+    )
+    swarm.compute_velocity(
+        best_particles=best_particles,
+        best_particle=best_particle,
+    )
+    swarm.step()
+
+    # test invalid particle dimensions
+    particles = torch.rand(n_particles, particle_dof + 1)
+    try:
+        swarm = LinearParticleSwarm(
+            particles=particles,
+            particle_bounds=particle_bounds,
+        )
+    except ValueError as e:
+        print(f"Expected and got error: {e}")
+    else:
+        raise ValueError("Expected ValueError")
+
+    # test invalid particle bounds
+    particle_bounds[:, 0] = 1.0
+    particle_bounds[:, 1] = -1.0
+    try:
+        swarm = LinearParticleSwarm(
+            particles=particles,
+            particle_bounds=particle_bounds,
+        )
+    except ValueError as e:
+        print(f"Expected and got error: {e}")
+    else:
+        raise ValueError("Expected ValueError")
+
+
+def test_particle_swarm_optimizer() -> None:
+    n_particles = 10
+    particle_dof = 2
+
+    bound = 500.0
+    particles = torch.rand(n_particles, particle_dof) * 2 * bound - bound
+    particle_bounds = torch.zeros(particle_dof, 2)
+    particle_bounds[:, 0] = -bound
+    particle_bounds[:, 1] = bound
+    particle_swarm = LinearParticleSwarm(
+        particles=particles,
+        particle_bounds=particle_bounds,
+    )
+    pso = ParticleSwarmOptimizer(
+        particle_swarm=particle_swarm,
+    )
+
+    capture_value = 2
+
+    def fitness_closure() -> torch.Tensor:
+        if capture_value != 2:
+            raise ValueError("Variable wasn't captured.")
+        return torch.square(pso.particle_swarm.particles).sum(dim=1)
+
+    best_particle = pso(fitness_function=fitness_closure, max_iterations=100)
+
+    if not torch.isclose(best_particle, torch.zeros(particle_dof), atol=1.0e-3).all():
+        raise ValueError("Expected best particle to be [0, 0].")
+
+
+if __name__ == "__main__":
+    # test_particle_swarm()
+    test_particle_swarm_optimizer()
